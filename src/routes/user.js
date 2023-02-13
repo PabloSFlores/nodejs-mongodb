1db48d61840c6ef70cc2074ea3f93e6bce0c364d
PabloSFlores <20213tn013@utez.edu.mx>
2023-02-12 21:34:29 -0600

    CRUD mongoDB
    Practica para proyecto

---
+++ src/routes/user.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const userSchema = require('../models/user')
+
+const router = express.Router()
+
+// create user
+router.post('/users', (req, res) => {
+    const user = userSchema(req.body)
+    user.save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+})
+
+// get all users
+router.get('/users', (req, res) => {
+    userSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+})
+
+// get one user
+router.get('/users/:id', (req, res) => {
+    const { id } = req.params
+    userSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+})
+
+//update user
+router.put('/users/:id', (req, res) => {
+    const { id } = req.params
+    const { name, age, email } = userSchema(req.body)
+    userSchema
+        .updateOne({ _id: id }, { $set: { name, age, email } })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+})
+
+//delete user
+router.delete('/users/:id', (req, res) => {
+    const { id } = req.params
+    userSchema
+        .deleteOne({_id: id})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+})
+
+module.exports = router
